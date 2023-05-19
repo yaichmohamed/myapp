580ae72798e806d5e8d23a80ab60d38f4aa2cfee
yaichmohamed <127444596+yaichmohamed@users.noreply.github.com>
2023-05-19 21:03:18 +0200

    Update Cardd.js

--- src/composant/Cardd.js
+++ src/composant/Cardd.js
@@ -6,47 +6,50 @@ import Form from 'react-bootstrap/Form';
 
 const Cardd = () => {
     return (
-      <div >
+      <div>
+        {/* Card component with a fixed width of 18rem */}
         <Card style={{ width: '18rem' }}>
+        {/* Card image */}
         <Card.Img variant="top" src={i1} />
         <Card.Body>
+          {/* Card title */}
           <Card.Title>Cristiano Ronaldo</Card.Title>
+          {/* Card text */}
           <Card.Text>
+          {/* Description of Cristiano Ronaldo */}
           Cristiano Ronaldo dos Santos Aveiro, couramment appelé Cristiano Ronaldo ou Ronaldo et surnommé CR7, né le 5 février 1985 à Funchal, est un footballeur international portugais qui évolue au poste d'attaquant à Al-Nassr FC
           </Card.Text>
           <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+            {/* Form component */}
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              {/* Email input field */}
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              {/* Password input field */}
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              {/* Checkbox input */}
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            
+            {/* Submit button */}
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Card.Body>
       </Card>
-</div>
+    </div>
     )
+}
 
-    
-
-    }
-
-  export default Cardd  
-    
-
-
-
-    
-
+export default Cardd;
